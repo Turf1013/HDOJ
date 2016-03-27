4800b56c1749fdf2604eab791f5f7666b4cd5ffb
Turf1013 <496528674@qq.com>
2016-03-27 15:40:18 +0800

    nearly finish with bugs fixing

--- acm_ant.py
+++ acm_ant.py
@@ -7,6 +7,9 @@ from acm_exchange import *
 def judge2OJ(pkuId, hduDes):
 	pkuCont = acmPku_getProblem(pkuId)
 	__, pkuDes = acmPku_parseProblemInfo(pkuCont)
+	
+	if not isinstance(pkuDes, str) or not isinstance(hduDes, str):
+		return False
 	hduDes = re.split('\s+', hduDes)
 	pkuDes = re.split('\s+', pkuDes)
 	mn = min(len(pkuDes), len(hduDes), 5)
@@ -16,15 +19,17 @@ def judge2OJ(pkuId, hduDes):
 	return True
 	
 	
-def fuzzySubmit(pid):
-	probCont = acmHdu_getProblem(pid)
+def fuzzySubmit(pid, hdu_opener):
+	retList = []
+	probCont = acmHdu_getProblem(pid, hdu_opener)
 	probTitle, probDes = acmHdu_parseProblemInfo(probCont)
 	probTitle = probTitle.strip()
 	logging.debug("[HDU] %s %s" % (pid, probTitle))
 	# search in pku
 	pkuSearchCont = acmPku_Search(probTitle)
+	# if not pkuSearchCont:
+		# return retList
 	probList = acmPku_getProblemList(pkuSearchCont)
-	retList = []
 	for (pkuId, pkuTitle) in probList:
 		pkuTitle = pkuTitle.strip()
 		if pkuTitle != probTitle:
@@ -34,48 +39,56 @@ def fuzzySubmit(pid):
 	return retList
 		
 
-def fuzzyMatch(hduUser, solved):
-	maxTime = 3
-	for pid in solved:
-		candList = fuzzySubmit(pid)
-		hduAcCont = acmHdu_getAcList(hduUser, pid)
+def fuzzyMatch(hduUser, hdu_solved, pku_solved, hdu_opener):
+	# maxTime = 10
+	for pid in hdu_solved:
+		candList = fuzzySubmit(pid, hdu_opener)
+		hduAcCont = acmHdu_getAcList(hduUser, pid, hdu_opener)
 		runId, lang = acmHdu_getAcCodeInfo(hduAcCont)
 		logging.debug("%s: %s" % (pid, str(candList)))
 		if candList:
-			code = acmHdu_getAcCode(runId)
-			print code
+			code = acmHdu_getAcCode(runId, hdu_opener)
 			for pkuId in candList:
-				logging.debug("[submit] %s %s %s" % (pid, lang, code))
-				acmPku_Submit(pkuId, lang, code)
-			maxTime -= 1
-			if maxTime <= 0:
-				break
+				if int(pkuId) in pku_solved:
+					break
+				logging.debug("[submit] %s %s %s" % (pid, lang, len(code)))
+				# sleep(2)
+				# acmPku_Submit(pkuId, lang, code)
+				pku_solved.add(int(pkuId))
+			# maxTime -= 1
+			# if maxTime <= 0:
+				# break
 		
 
 if __name__ == "__main__":
-	desFileName = "F:\Qt_prj\hdoj\data.out"
+	desFileName = "F:\code_today\data.in"
 				
-	hdu_user = "Trasier"
+	hdu_user = "Bombe16"
 	pku_user = "Bombe1013"
 	password = "496528674"
 	initLog()
 	logging.debug("hdu login...")
-	acmHdu_Login(hdu_user, password)
+	hdu_opener = acmHdu_Login(hdu_user, password)
 	logging.debug("pku login...")
-	acmPku_Login(pku_user, password)
-	acmPku_Submit(1000, "C++", CFP.a_plus_b)
+	pku_opener = acmPku_Login(pku_user, password)
 	
 	logging.debug("hdu get user info...")
-	userInfo = acmHdu_getUserInfo(hdu_user)
-	with open(desFileName, "w") as fout:
-		fout.write(userInfo)
+	hdu_userInfoCont = acmHdu_getUserInfo(hdu_user, hdu_opener)
+	
+	logging.debug("pku get user info...")
+	pku_userInfoCont = acmPku_getUserInfo(pku_user)
 		
 	logging.debug("hdu get solved...")
-	solvedList = acmHdu_parseSolved(userInfo)
-	print "solved =", len(solvedList)
+	hdu_solvedList = acmHdu_parseSolved(hdu_userInfoCont)
+	print "|hdu_solved| =", len(hdu_solvedList)
+	
+	logging.debug("pku get solved...")
+	pku_solvedList = acmPku_parseSolved(pku_userInfoCont)
+	pku_solvedSet = set(map(int, pku_solvedList))
+	print "|pku_solved| =", len(pku_solvedList)
 	
 	logging.debug("matching in pku...")
-	fuzzyMatch(hdu_user, solvedList)
+	fuzzyMatch(hdu_user, hdu_solvedList, pku_solvedSet, hdu_opener)
 	
 	logging.debug("ending all...")
 	
--- acm_exchange.py
+++ acm_exchange.py
@@ -1,6 +1,7 @@
 #!/usr/env python
 
 import os
+from datetime import date
 import httplib
 import urllib2
 import urllib
@@ -46,6 +47,7 @@ class constForPku:
 	searchTitle_xpath = "/html/body/center[2]/table/tr"
 	probTitle_xpath = "/html/body/table[2]/tr/td/div[2]"
 	describe_xpath = "/html/body/table[2]/tr/td/div[4]"
+	solved_xpath = "/html/body/center/table/tr[3]/td[3]/script"
 	langDict = {
 		"G++"		: 0,
 		"GCC"		: 1,
@@ -76,12 +78,12 @@ class CFP(constForPku):
 
 def initLog():
 	# os.chdir(parentDirPath)
-	suffix = 'acm_exchange'
+	suffix = 'v%02d%02d' % (date.today().month, date.today().day)
 	logging.basicConfig(
 				level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
-                filename=__file__[:-3]+'_'+suffix+'.log',
+                filename=__file__[:__file__.rindex('.')]+'_'+suffix+'.log',
                 filemode='w')
 	
 def acmHdu_Login(user, password):
@@ -95,21 +97,29 @@ def acmHdu_Login(user, password):
 		data = urllib.urlencode({"username":user, "userpass":password})
 		urllib2.install_opener(opener)
 		urllib2.urlopen(login_page, data)
+		return opener
 		
 	except Exception, e:
 		print "Login unsucceed"
 		print e
 		
-def acmHdu_getUserInfo(user):
+		
+def acmHdu_getUserInfo(user, opener=None):
 	info_url = urljoin(CFH.mainPage, "userstatus.php?user="+user)
 	print info_url
-	content = urllib2.urlopen(info_url).read()
+	if opener:
+		content = opener.open(info_url).read()
+	else:
+		content = urllib2.urlopen(info_url).read()
 	return content
 	
 	
-def acmHdu_getProblem(pid):
+def acmHdu_getProblem(pid, opener=None):
 	problem_url = CFH.mainPage + "/showproblem.php?pid=" + str(pid)
-	content = urllib2.urlopen(problem_url).read()
+	if opener:
+		content = opener.open(problem_url).read()
+	else:
+		content = urllib2.urlopen(problem_url).read()
 	return content
 	
 	
@@ -122,9 +132,12 @@ def acmHdu_parseProblemInfo(content):
 	return (title, des)
 	
 
-def acmHdu_getAcList(user,pid):	
+def acmHdu_getAcList(user,pid, opener=None):	
 	ac_url = CFH.mainPage + "/status.php?first=&pid=%s&user=%s&lang=0&status=5" % (str(pid), user)
-	content = urllib2.urlopen(ac_url).read()
+	if opener:
+		content = opener.open(ac_url).read()
+	else:
+		content = urllib2.urlopen(ac_url).read()
 	return content
 	
 	
@@ -137,30 +150,39 @@ def acmHdu_getAcCodeInfo(content):
 	return (runId, lang)
 	
 	
-def acmHdu_getAcCode(runId):
+def acmHdu_getAcCode(runId, opener=None):
 	code_url = CFH.mainPage + "/viewcode.php?rid=" + str(runId)
-	content = urllib2.urlopen(code_url).read()
-	print content
+	if opener:
+		content = opener.open(code_url).read()
+	else:
+		content = urllib2.urlopen(code_url).read()
+	# print content
 	tree = etree.HTML(content)
 	eleList = tree.xpath(CFH.code_xpath)
 	code = eleList[0].text if eleList else ""
-	codeList = filter(lambda s:s.strip(), code.split("\n"))
-	return "".join(codeList)
+	codeList = filter(lambda s:s.strip(), code.split("\r\n"))
+	return "\n".join(codeList)
 		
 	
-def acmHdu_Submit(pid, lang, code):
+def acmHdu_Submit(pid, lang, code, opener=None):
 	submit_url = urljoin(CFH.mainPage + "/submit.php?action=submit")
 	data = urllib.urlencode({"problemid":pid, "language":lang, "usercode":code})
 	req = urllib2.Request(submit_url, data)
-	response = urllib2.urlopen(req)
+	if opener:
+		response = opener.open(req)
+	else:
+		response = urllib2.urlopen(req)
 	
 	
-def acmHdu_Search(keyword):
+def acmHdu_Search(keyword, opener=None):
 	search_url = urljoin(CFH.mainPage, "search.php?action=listproblem")
 	data = urllib.urlencode({"content":keyword.strip(), "searchmode":"title"})
 	try:
 		req = urllib2.Request(search_url, data)
-		response = urllib2.urlopen(req)
+		if opener:
+			response = opener.open(req)
+		else:
+			response = urllib2.urlopen(req)
 		content = response.read()
 		return  content
 		
@@ -176,10 +198,13 @@ def acmHdu_parseSolved(content):
 	return solvedList	
 
 	
-def acmHdu_Status_Parse(user, pid, lang):
+def acmHdu_Status_Parse(user, pid, lang, opener=None):
 	sleep(5)
 	status_url = urljoin(CFH.mainPage, "status.php?first=&pid=%s&user=%s&lang=%s&status=0" % (str(pid), user, str(lang+1)))
-	content = urllib2.urlopen(status_url).read()
+	if opener:
+		content = opener.open(status_url).read()
+	else:
+		content = urllib2.urlopen(status_url).read()
 	tree = etree.HTML(content)
 	infoList = map(
 		lambda ele: ele.text, tree.xpath(CFH.judgeStatus_xpath)
@@ -187,10 +212,13 @@ def acmHdu_Status_Parse(user, pid, lang):
 	return infoList[0] if infoList else ""
 	
 	
-def acmHdu_ExeTime_Parse(user, pid, lang):
+def acmHdu_ExeTime_Parse(user, pid, lang, opener=None):
 	sleep(5)
 	status_url = urljoin(CFH.mainPage, "status.php?first=&pid=%s&user=%s&lang=%s&status=0" % (str(pid), user, str(lang+1)))
-	content = urllib2.urlopen(status_url).read()
+	if opener:
+		content = opener.open(status_url).read()
+	else:
+		content = urllib2.urlopen(status_url).read()
 	tree = etree.HTML(content)
 	statusList = map (
 		lambda ele: ele.text, tree.xpath(CFH.judgeStatus_xpath)
@@ -214,6 +242,7 @@ def acmPku_Login(user, password):
 		data = urllib.urlencode({"B1":"login", "password1":password, "user_id1":user, "url":"."})
 		urllib2.install_opener(opener)
 		loginSt = urllib2.urlopen(login_page, data).read()
+		return opener
 		
 	except Exception, e:
 		print "Login unsucceed"
@@ -239,7 +268,11 @@ def acmPku_Submit(pid, lang, code):
 
 def acmPku_Search(keyword):
 	search_url = urljoin(CFP.mainPage, "searchproblem")
-	data = urllib.urlencode({"B1":"GO", "field":"title", "key":keyword.strip()})
+	try:
+		data = urllib.urlencode({"B1":"GO", "field":"title", "key":keyword.strip()})
+	except:
+		logging.debug("[keyword-Error]: %s" % (keyword))
+		return ""
 	try:
 		req = urllib2.Request(search_url, data)
 		response = urllib2.urlopen(req)
@@ -255,6 +288,24 @@ def acmPku_getProblem(pid):
 	content = urllib2.urlopen(problem_url).read()
 	return content
 	
+def acmPku_getUserInfo(user, opener=None):
+	status_url = CFP.mainPage + "/userstatus?user_id=" + str(user)
+	if opener:
+		content = opener.open(status_url).read()
+	else:
+		content = urllib2.urlopen(status_url).read()
+	return content
+	
+def acmPku_parseSolved(content):
+	tree = etree.HTML(content)
+	eleList = tree.xpath(CFP.solved_xpath)
+	js_code = eleList[0].text
+	lines = js_code.split("\n")
+	retList = []
+	for line in lines:
+		if line.startswith('p('):
+			retList.append(line[2:-1])
+	return retList
 	
 def acmPku_parseProblemInfo(content):
 	tree = etree.HTML(content)
@@ -281,9 +332,8 @@ def acmPku_getProblemList(content):
 
 	
 def acmPku_Status_Parse(user, pid):
-	sleep(5)
-	status_url = urljoin(url, "status") + "?"
-	data = urllib.urlencode({"problem_id":pid, "user_id":user})
+	sleep(3)
+	status_url = CFP.mainPage + "/status?problem_id=%s&user_id=%s" % (pid, user)
 	content = urllib2.urlopen(status_url).read()
 	tree = etree.HTML(content)
 	statusList = map(
@@ -292,11 +342,12 @@ def acmPku_Status_Parse(user, pid):
 	return statusList[0] if statusList else ""
 	
 		
-def acmPku_ExeTime_Parse(user, pid, lang):
-	sleep(5)
-	status_url = urljoin(url, "status") + "?"
-	data = urllib.urlencode({"problem_id":pid, "user_id":user})
+def acmPku_ExeTime_Parse(user, pid):
+	sleep(3)
+	status_url = CFP.mainPage + "/status?problem_id=%s&user_id=%s" % (pid, user)
 	content = urllib2.urlopen(status_url).read()
+	with open("F:\code_today\data.in", "w") as fout:
+		fout.write(content)
 	tree = etree.HTML(content)
 	statusList = map(
 		lambda ele: ele.text, tree.xpath(CFP.judgeStatus_xpath)
@@ -306,39 +357,47 @@ def acmPku_ExeTime_Parse(user, pid, lang):
 		lambda ele: ele.text, tree.xpath(CFP.exeTime_xpath)
 	)
 	etime = int(etimeList[0][:-2]) if etimeList else 10**9
-	return status.strip(),etime
+	return status.strip(), etime
 	
 	
 def acmPku_SubmitOnce(user, pid, lang, code):
 	acmPku_Submit(pid, lang, code)
-	print "submit %d" % (i)
+	# print "submit %d" % (pid)
 	while True:
-		status,etime = acmPku_ExeTime_Parse(user, pid, lang)
-		if status=="Waiting":
+		status = acmPku_Status_Parse(user, pid)
+		status = status.strip()
+		print status
+		if status=="Waiting" or status=="Compiling" or status.startswith("Running"):
 			continue
-		logging.debug("%d: %s, %d" % (pid, status, etime))
+		logging.debug("%s: %s" % (pid, status))
 		break
-	return mintime
+	return status
 	
 	
 if __name__ == "__main__":
+	initLog()
+	
 	hdu_user = "Bombe16"
 	pku_user = "Bombe1013"
 	password = "496528674"
-	acmHdu_Login(hdu_user, password)
-	initLog()
+	# logging.debug("hdu login begin...")
+	# hdu_opener = acmHdu_Login(hdu_user, password)
+	
 	logging.debug("pku login begin...")
 	acmPku_Login(pku_user, password)
 	# logging.debug("pku submit begin...")
-	acmPku_Submit(1000, "C++", CFP.a_plus_b)
+	# acmPku_Submit(1000, "C++", CFP.a_plus_b)
 	# content = acmPku_Search("Problem")
 	# with open("F:\code_today\data.in", "w") as fout:
 		# fout.write(content)
 	# logging.debug("pku submit end...")
 	# parse_status()
-	hduAcCont = acmHdu_getAcList(hdu_user, 1000)
-	runId, lang = acmHdu_getAcCodeInfo(hduAcCont)
-	codeCont = acmHdu_getAcCode(runId)
-	with open("F:\code_today\data.in", "w") as fout:
-		fout.write(codeCont)
-		
+	# hduAcCont = acmHdu_getAcList(hdu_user, 1000)
+	# runId, lang = acmHdu_getAcCodeInfo(hduAcCont)
+	# codeCont = acmHdu_getAcCode(runId)
+	# codeCont = acmHdu_getAcCode(16611787, hdu_opener)
+	# with open("F:\code_today\data.in", "w") as fout:
+		# fout.write(codeCont)
+	status = acmPku_SubmitOnce(pku_user, 1000, "C++", CFP.a_plus_b)
+	print status
+	
